9145ad06149f6b6f142a9b29945c8facb23fd45e
baoanh2809 <88428091+baoanh2809@users.noreply.github.com>
2021-11-26 10:24:56 +0700

    day2
    @ZeroRyo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        redEF: "#EF4444",
+        ra50: "50%",
+        pA7: "#A78BFA",
+        70: "70px",
+        50:"50px",
+      }
+    },
   },
   variants: {
     extend: {},
